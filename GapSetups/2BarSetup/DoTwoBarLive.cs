d404d1351d95d847dc0d0bc0564cfbd02270e1dc
jmott <jmott@surgepays.com>
2025-10-05 17:16:06 -0500

    modify 2 bar

--- GapSetups/2BarSetup/DoTwoBarLive.cs
+++ GapSetups/2BarSetup/DoTwoBarLive.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using MIConvexHull;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Trainers.FastTree;
-using MIConvexHull;
 using SchwabLib;
 using SchwabLib.Models;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 
 namespace CandlePatternML
@@ -19,43 +20,34 @@ namespace CandlePatternML
             int length = model.candles.Length;
             List<Candle> PatternCandles = model.candles.Skip(length - 3).ToList();
 
-            // do we have a gap?
-            if (PatternCandles[1].open <= PatternCandles[0].close) return new MLResult(model,false, 0);
-            // did the close of today fill the gap?
-            if (PatternCandles[1].close <= PatternCandles[0].close) return new MLResult(model,false, 0);
-
-            // does the hold bar hold above the gap bar? 
-            // only the low can dip below the gap bar low
-            if (PatternCandles[2].close <= PatternCandles[0].low) return new MLResult(model,false, 0);
-            if (PatternCandles[2].high <= PatternCandles[0].low) return new MLResult(model, false, 0);
-            if (PatternCandles[2].open <= PatternCandles[0].low) return new MLResult(model, false, 0);
-
-#if false
-// use for graphing the patterns
-            List<ThreeBarPatternModel> patternModelList = new List<ThreeBarPatternModel>();
-            List<DateTime> dtList = new List<DateTime>();
-            List<CandlePatternOutput> outputList = new List<CandlePatternOutput>();
-#endif
-
-            Console.WriteLine($"found gap at  {PatternCandles[1].dtDotNet.ToShortDateString()}");
-            TwoBarPatternModel input = new TwoBarPatternModel
+            double margin = 0.001;   // tolerance for comparing highs and lows
+            Candle bar0 = PatternCandles[0]; // bar before the gap
+                                             // is the day before gap a red day?
+            string notice = "";
+            if (bar0.close < bar0.open)
             {
-                GapBarLowHigh = new LowHighModel(PatternCandles[1].low, PatternCandles[1].high),
-                GapCandle = PatternCandles[1],
+                notice = "Red day before gap up day";
+            }
+            Candle bar1 = PatternCandles[1]; // gap day
+            Candle bar2 = PatternCandles[2]; // bar after gap (candidate inside bar)
+            
+            if (Math.Log((double) bar1.open / (double) bar0.close) > .0068 && bar1.close > bar1.open)
+            {
+                double upperLimit = (double) bar1.high * (1 + margin);
+                double lowerLimit = (double) bar1.low * (1 - margin);
+                
+                bool BelowUpperLimit = (double)bar2.high <= upperLimit;
+                bool AboveLowerLimit = (double)bar2.low >= lowerLimit;
 
-                Hold1BarLowHigh = new LowHighModel(PatternCandles[2].low, PatternCandles[2].high),
-                Hold1Candle = PatternCandles[2],
+                if (BelowUpperLimit && AboveLowerLimit)
+                {
+                    // inside bar
+                    return new MLResult(model, true, 1, notice);
+                }
+            }
+            
+            return new MLResult(model,false, 0);
 
-            };
-            // add in extra features that emphasize some aspects of the pattern
-            input.SetExtraFeatures();
-            // run the prediction
-            var result = mlEngine.predictionEngine2Bar.Predict(input);
-
-            Console.WriteLine($"Prediction for {model.symbol} : {(result.IsMatch ? "MATCH" : "NO MATCH")}, Probability: {result.Probability:P1}");
-
-
-            return new MLResult(model,result.IsMatch, result.Probability);
         }
     }
 }
--- Models/MLResult.cs
+++ Models/MLResult.cs
@@ -11,16 +11,18 @@ namespace CandlePatternML
 {
     public class MLResult
     {
-        public MLResult(GetCandleModel model, bool success, float confidence)
+        public MLResult(GetCandleModel model, bool success, float confidence,string notice="")
         {
             CandleModel = model;
             Success = success;
             Confidence = confidence;
+            Notice = notice;
         }
 
         public GetCandleModel CandleModel { get; set; }
         public string Ticker { get { return CandleModel.symbol; } }
         public bool Success { get; set; }
         public float Confidence { get; set; }
+        public string Notice { get; set; } = "";
     }
 }
