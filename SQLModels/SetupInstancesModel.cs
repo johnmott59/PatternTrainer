5f0ae09881334ee9bc4a14694af9b9b5c2efbcf3
jmott <jmott@surgepays.com>
2026-01-03 09:39:24 -0600

    updates

--- SQLModels/SetupInstancesModel.cs
+++ SQLModels/SetupInstancesModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CandlePatternML
 {
@@ -10,6 +12,39 @@ namespace CandlePatternML
         public int ID { get; set; }
         public string Ticker { get; set; }
         public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Constructor for SetupInstancesModel
+        /// </summary>
+        /// <param name="ticker">The ticker symbol</param>
+        /// <param name="dateCreated">The date when the setup instance was created</param>
+        public SetupInstancesModel(string ticker, DateTime dateCreated)
+        {
+            Ticker = ticker;
+            DateCreated = dateCreated;
+        }
+
+        /// <summary>
+        /// Saves this SetupInstancesModel to the SetupInstances table in the database
+        /// </summary>
+        public void Save()
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["StockSetup"].ConnectionString))
+            {
+                connection.Open();
+                string insertQuery = @"INSERT INTO [SetupInstances] ([Ticker], [DateCreated])
+                                      OUTPUT INSERTED.ID
+                                      VALUES (@Ticker, @DateCreated)";
+                
+                using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Ticker", Ticker);
+                    command.Parameters.AddWithValue("@DateCreated", DateCreated);
+                    
+                    ID = (int)command.ExecuteScalar();
+                }
+            }
+        }
     }
 }
 
