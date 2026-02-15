383253b7ce42dbc3db292a89330579a46c4f80d7
jmott <jmott@surgepays.com>
2026-02-15 13:31:42 -0600

    add conflience indicator

---
+++ ConfluenceSetup/DoConfluence.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers.FastTree;
+using MIConvexHull;
+using SchwabLib;
+using SchwabLib.Models;
+using System.Diagnostics.Eventing.Reader;
+using System.DirectoryServices.ActiveDirectory;
+using System.Windows.Input;
+using System.Data;
+
+namespace CandlePatternML
+{
+    public partial class Program
+    {
+        public MLResult DoConfluence(GetCandleModel model)
+        {
+            double ProximityATRMultiplier = 1.5; // This can be adjusted based on how close you want the price to be to the indicators
+
+            // get the candles for the last 6 months.
+            var candles = model.candles.Where(c => c.dtDotNet > DateTime.Now.AddMonths(-6)).ToList();
+            
+            // get studies for this time
+
+            List<double?> emaSlowSeries = SchwabLib.Studies.CalculateEMA(candles, 200);
+            List<double?> emaFastSeries = SchwabLib.Studies.CalculateEMA(candles, 34);
+            List<double?> atrSeries = SchwabLib.Studies.CalculateATR(candles, 14);
+
+            int conflenceCount = 0;
+            int lastindex = candles.Count - 1;
+
+            if (atrSeries[lastindex] == null) return new MLResult(model, false, 0) ;
+
+            double currentPrice = (double) candles[lastindex].close; 
+            
+            double currentATR = atrSeries[lastindex].Value;
+            double proximityThreshold = ProximityATRMultiplier * currentATR;
+
+            bool nearEMASlow = Math.Abs((double) (currentPrice - emaSlowSeries[lastindex])) <= proximityThreshold;
+            bool nearEMAFast = Math.Abs((double) (currentPrice - emaFastSeries[lastindex])) <= proximityThreshold;
+
+            int confluenceCount =  (nearEMASlow ? 1 : 0) +
+                                    (nearEMAFast ? 1 : 0) ;
+
+            if (confluenceCount >= 1) return new MLResult(model, true, confluenceCount);
+
+            return new MLResult(model, false, 0);
+
+        }
+    }
+}
+
--- Main.cs
+++ Main.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Trainers.FastTree;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SchwabLib;
 using SchwabLib.Models;
+using SchwabLib.OptionModel;
 using System;
 using System.Collections.Generic;
-
+using static SchwabLib.TheilSenEstimator;
 
 namespace CandlePatternML
 {
@@ -22,9 +25,92 @@ namespace CandlePatternML
             //   p.DoFourBarTraining();
             //   p.DoFiveBarTraining();
 
-          //  p.RunHistory().Wait();
+            //  p.RunHistory().Wait();
 
-           p.Run().Wait();
+            p.Run().Wait();
+
+           // p.GetDividend();
+
+
+        }
+
+
+            List<string> tickers = new List<string>
+        {
+
+"ABR",
+"ACP",
+"ADAMI",
+"ADX",
+"ARCC",
+"ASGI",
+"AVK",
+"BITO",
+"BKLN",
+"BTCI",
+"CAIE",
+"CEFS",
+"CLOZ",
+"CSWC",
+"EIC",
+"FSCO",
+"GSBD",
+"HTD",
+"HTGC",
+"IYRI",
+"JBBB",
+"JEPI",
+"JLS",
+"NCV",
+"NIE",
+"NML",
+"PAXS",
+"PBDC",
+"PCN",
+"PDI",
+"PFFA",
+"PTY",
+"STWD",
+"SVOL",
+"THQ",
+
+        };
+        public void GetDividend()
+        {
+            string authKey = GetAuthKey();
+
+            List<(string, decimal)> results = new List<(string, decimal)>();
+
+            foreach (var t in tickers)
+            {
+                string quoteData = oAPIWrapper.GetFullQuotedata(authKey, t);
+
+                JObject root = JObject.Parse(quoteData);
+
+                foreach (var symbolProperty in root.Properties())
+                {
+                    string symbol = symbolProperty.Name;
+                    JObject data = (JObject)symbolProperty.Value;
+
+                    decimal? divAmount = data["fundamental"]?["divAmount"]?.Value<decimal>();
+                    int? divFreq = data["fundamental"]?["divFreq"]?.Value<int>();
+                    decimal? divPayAmount = data["fundamental"]?["divPayAmount"]?.Value<decimal>();
+                    decimal? lastPrice = data["quote"]?["lastPrice"]?.Value<decimal>();
+
+                    decimal AnnulizedDividend = divPayAmount.Value * divFreq.Value;
+                    decimal DividendYield = (AnnulizedDividend / lastPrice.GetValueOrDefault()) * 100;
+
+                    results.Add((symbol, DividendYield));
+                }
+
+                results = results.OrderByDescending(r => r.Item2).ToList();
+      
+            }
+
+            foreach (var r in results.OrderByDescending(r => r.Item2))
+            {
+                Console.WriteLine($"{r.Item1}: {r.Item2:F2}%");
+            }
 
 
         }
